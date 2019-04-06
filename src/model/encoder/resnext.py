893c71b73e4a9d911fb2b1aa9ecade11c3b81424
AntonPon <ponomarchuk.anton@gmail.com>
2019-04-07 00:44:34 +0300

    added building block for resnet

--- src/model/encoder/resnext.py
+++ src/model/encoder/resnext.py
@@ -20,21 +20,29 @@ class SeparableConv2d(nn.Module):
 
 
 class BuildingBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel=3, stride=1, padding=0, dilation=1, bias=True):
+    def __init__(self, in_channels, out_channels, kernel=3):
         super().__init__()
-        self.sepConv1 = SeparableConv2d(in_channels, out_channels, kernel=kernel, stride=stride,
-                                        padding=padding, dilation=dilation, bias=bias)
+        self.sepConv1 = SeparableConv2d(in_channels, out_channels, kernel=kernel)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.sepConv2 = SeparableConv2d(out_channels, out_channels, kernel=kernel, stride=stride,
-                                        padding=padding, dilation=dilation, bias=bias)
+        self.sepConv2 = SeparableConv2d(out_channels, out_channels, kernel=kernel)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.sepConv3 = SeparableConv2d(out_channels, out_channels, kernel=kernel, stride=2,
-                                        padding=padding, dilation=dilation, bias=bias)
+        self.sepConv3 = SeparableConv2d(out_channels, out_channels, kernel=kernel, stride=2)
         self.bn3 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
 
     def forward(self, input_batch):
-        pass
+        input_batch = self.sepConv1(input_batch)
+        input_batch = self.bn1(input_batch)
+        input_batch = self.relu(input_batch)
+
+        input_batch = self.sepConv2(input_batch)
+        input_batch = self.bn2(input_batch)
+        input_batch = self.relu(input_batch)
+
+        input_batch = self.sepConv3(input_batch)
+        input_batch = self.bn3(input_batch)
+        return self.relu(input_batch)
+
 
 class EntryFlow(nn.Module):
     def __init__(self):
